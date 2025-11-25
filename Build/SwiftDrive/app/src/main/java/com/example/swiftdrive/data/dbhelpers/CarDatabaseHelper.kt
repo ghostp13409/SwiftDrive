90ee5a324fa2a02bbf849026f701e0590390fc16
Car1osF1ores <c2005f08@hotmail.com>
2025-11-25 18:24:54 -0500

    Implement car management features in SwiftDrive app
    Added Car model enhancements, database helper for CRUD operations, and CarsViewModel for state management. Introduced CarCard component, AddCarScreen, CarScreen, and CarDetailScreen for car listing and details. Updated dependencies and project configuration to support new features.

--- Build/SwiftDrive/app/src/main/java/com/example/swiftdrive/data/dbhelpers/CarDatabaseHelper.kt
+++ Build/SwiftDrive/app/src/main/java/com/example/swiftdrive/data/dbhelpers/CarDatabaseHelper.kt
@@ -1,4 +1,81 @@
 package com.example.swiftdrive.data.dbhelpers
 
-class CarDatabaseHelper {
+import android.R
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.example.swiftdrive.data.models.Car
+
+class CarDatabaseHelper(context : Context) :
+    SQLiteOpenHelper(context, "cars.db", null, 1){
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val createTableQuery = """
+            CREATE TABLE cars(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                year INTEGER,
+                make TEXT,
+                model TEXT,
+                pricePerDay REAL,
+                isAvailable INTEGER,
+                engineType TEXT,
+                condition TEXT,
+                category TEXT
+            )
+        """.trimIndent()
+        db?.execSQL(createTableQuery)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.execSQL("DROP TABLE IF EXISTS cars")
+        onCreate(db)
+    }
+
+    fun insertCar(year: String, make: String, model: String, pricePerDay: String, isAvailable: Boolean, engineType: String, condition: String, category: String) {
+        val db = writableDatabase
+        val values = ContentValues().apply {
+            put("year", year)
+            put("make", make)
+            put("model", model)
+            put("pricePerDay", pricePerDay)
+            put("isAvailable", if (isAvailable) 1 else 0)
+            put("engineType", engineType)
+            put("condition", condition)
+            put("category", category)
+        }
+        db.insert("cars", null, values)
+        db.close()
+    }
+
+    fun getAllCars(): List<Car> {
+        val cars = mutableListOf<Car>()
+        val db = readableDatabase
+        val cursor = db.rawQuery("SELECT * FROM cars", null)
+        while (cursor.moveToNext()) {
+            cars.add(
+                Car(
+                    id = cursor.getInt(0),
+                    year = cursor.getInt(1),
+                    make = cursor.getString(2),
+                    model = cursor.getString(3),
+                    pricePerDay = cursor.getDouble(4),
+                    isAvailable = cursor.getInt(5) == 1,
+                    engineType = cursor.getString(6),
+                    condition = cursor.getString(7),
+                    category = cursor.getString(8)
+                )
+            )
+        }
+        cursor.close()
+        db.close()
+        return cars
+    }
+
+    fun deleteCar(id: Int){
+        val db = writableDatabase
+        db.delete("cars", "id=?", arrayOf(id.toString()))
+        db.close()
+    }
+
 }
--- Build/SwiftDrive/app/src/main/java/com/example/swiftdrive/features/cars/CarScreen.kt
+++ Build/SwiftDrive/app/src/main/java/com/example/swiftdrive/features/cars/CarScreen.kt
@@ -1,9 +1,41 @@
 package com.example.swiftdrive.features.cars
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.swiftdrive.components.CarCard
+
 
 @Composable
-fun CarScreen() {
-    Text("Car Screen")
+fun CarScreen(
+    viewModel: CarsViewModel = viewModel(),
+    modifier: Modifier = Modifier,
+    onEventClick: () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(25.dp)
+    ) {
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            items(viewModel.cars.value) { car ->
+                CarCard(
+                    car = car,
+                    onEventClick = {
+                        viewModel.selectCar(car)
+                        onEventClick()
+                    }
+                )
+            }
+        }
+    }
 }
